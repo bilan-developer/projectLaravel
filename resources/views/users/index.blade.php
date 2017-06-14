36b05a63555bbaeb626b68db2f61cd4fe300f955
Sasha <hagrida@yandex.ru>
2017-06-14 10:15:19 +0300

    Развернул Laravel, добавил авторизацию, сделал вывод пользователей

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                  <p>Все пользователи</p>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="panel-body">
+
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <tr >
+                                        <td>№</td>
+                                        <td>Email</td>
+                                        <td>login</td>
+                                    </tr>
+                                    @foreach($users as $user)
+                                    <tr>
+                                        <td> {{$user->id}}</td>
+                                        <td> {{$user->name}}</td>
+                                        <td> {{$user->email}}</td>
+                                    </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
